d899f386cefe24ed87366c2e5fa0b0f27187380f
burcakozgul <burcakozgul@gmail.com>
2020-01-21 20:13:59 +0300

    TypeError and TaskException

--- Example/src/ManagementSystem/ManagementSystemMain.java
+++ Example/src/ManagementSystem/ManagementSystemMain.java
@@ -2,49 +2,67 @@ package ManagementSystem;
 
 import java.util.Scanner;
 
-
 public class ManagementSystemMain {
 	static TaskManagementSystem tms;
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		tms = TaskManagementSystem.getIntsance();
-		
+
 		tms.createUser("Burcak", UserType.DEVELOPER);
-		tms.createTask("PAYE", 0, TaskStatus.ANALYSIS, TaskPriority.HIGH );
+		tms.createTask("PAYE", 0, TaskStatus.ANALYSIS, TaskPriority.HIGH);
 		Scanner scan = new Scanner(System.in);
-		int count=0;
-		while(count<4) {
+		int count = 0;
+		while (true) {
 			printMainMenu();
-			switch(getUserIntInput(scan)){
+			try {
+				switch (getUserIntInput(scan)) {
 				case 1:
-					createUser(scan,tms);
+					createUser(scan, tms);
 					break;
 				case 2:
-					createTask(scan,tms);
+					createTask(scan, tms);
 					break;
 				case 3:
-					updateTaskPriorty(scan,tms);
+					updateTaskPriorty(scan, tms);
 					break;
 				case 4:
-					updateTaskStatus(scan,tms);
+					updateTaskStatus(scan, tms);
 					break;
 				case 5:
 					updateTaskUser(scan, tms);
 					break;
 				case 6:
-					count=4;
-					System.out.println("Programdan çıkılıyor...");
+					System.out.println("Exit the program");
 					return;
 				default:
 					count++;
 					System.out.println("invalid value");
 					System.out.println("---------------------------------------");
-					if(count==4){
-						System.out.println("Programdan çıkılıyor...");
+					if (count == 4) {
+						System.out.println("Exit the program");
 					}
-					break;	
-			}
+					break;
+				}
+			} 
+			catch(TaskException e) {
+				print(e);
+			} catch(Exception e){
+				print(e);
+				return;
+			} 
 		}
 	}
+
+	private static void print(Object o) {
+		if (o instanceof String) {
+			System.out.println("Hatali giris yaptiniz");
+		}
+		if (o instanceof TypeError) {
+			System.out.println("task yok");
+		}
+		
+	}
+
 	private static void printMainMenu() {
 		System.out.println("1-Create User");
 		System.out.println("2-Create Task");
@@ -54,87 +72,63 @@ public class ManagementSystemMain {
 		System.out.println("6-Exit");
 		System.out.println("Please choose progress: ");
 	}
-	private static void createUser(Scanner scan, TaskManagementSystem tms) {
-		
-		try {
-			System.out.println("Enter name: ");
-			String name =scan.next();
-			System.out.println("Enter user type: ");
-			System.out.println("ANALYST, DEVELOPER, TESTER");
-			String type = scan.next();
-			tms.createUser(name, UserType.valueOf(type));
-		} 
-		catch (Exception e) {
- 			System.out.println("Yalnış giriş yaptınız. Çıkışa yönlendiriliyorsunuz.");
- 			System.out.println("---------------------------------------");
-		}
+
+	private static void createUser(Scanner scan, TaskManagementSystem tms) throws Exception {
+		System.out.println("Enter name: ");
+		String name = scan.next();
+		System.out.println("Enter user type: ");
+		System.out.println("ANALYST, DEVELOPER, TESTER");
+		String type = scan.next();
+		tms.createUser(name, UserType.valueOf(type));
 	}
-	private static void createTask(Scanner scan, TaskManagementSystem tms) {
-		try {
-			System.out.println("Enter task name: ");
-			String name =scan.next();
-			scan.nextLine();
-			tms.getAllUsers();
-			System.out.println("Enter user id: ");
-			int userId = scan.nextInt();
-			System.out.println("Enter task status: ");
-			System.out.println("ANALYSIS, DEVELOPMENT, TEST");
-			String status=scan.next();
-			System.out.println("Enter priority status: ");
-			System.out.println("LOW, MEDIUM, HIGH");
-			String priority=scan.next();
-			tms.createTask(name, userId, TaskStatus.valueOf(status), TaskPriority.valueOf(priority));
-			
-		} catch (Exception e) {
-			System.out.println("Yalnış giriş yaptınız. Çıkışa yönlendiriliyorsunuz.");
- 			System.out.println("---------------------------------------");
-		}
+
+	private static void createTask(Scanner scan, TaskManagementSystem tms) throws Exception {
+		System.out.println("Enter task name: ");
+		String name = scan.next();
+		scan.nextLine();
+		tms.getAllUsers();
+		System.out.println("Enter user id: ");
+		int userId = scan.nextInt();
+		System.out.println("Enter task status: ");
+		System.out.println("ANALYSIS, DEVELOPMENT, TEST");
+		String status = scan.next();
+		System.out.println("Enter priority status: ");
+		System.out.println("LOW, MEDIUM, HIGH");
+		String priority = scan.next();
+		tms.createTask(name, userId, TaskStatus.valueOf(status), TaskPriority.valueOf(priority));
 	}
-	private static void updateTaskPriorty(Scanner scan, TaskManagementSystem tms) {
-		try {
-			tms.getAllTasks();
-			System.out.println("Enter task id: ");
-			int taskId=scan.nextInt();
-			System.out.println("Enter priority status: ");
-			System.out.println("LOW, MEDIUM, HIGH");
-			String priority=scan.next();
-			tms.updatePriority(taskId, TaskPriority.valueOf(priority));
-		} catch (Exception e) {
-			System.out.println("Yalnış giriş yaptınız. Çıkışa yönlendiriliyorsunuz.");
- 			System.out.println("---------------------------------------");
-		}
-		
+
+	private static void updateTaskPriorty(Scanner scan, TaskManagementSystem tms) throws TaskException {
+		tms.getAllTasks();
+		System.out.println("Enter task id: ");
+		int taskId = scan.nextInt();
+		System.out.println("Enter priority status: ");
+		System.out.println("LOW, MEDIUM, HIGH");
+		String priority = scan.next();
+		tms.updatePriority(taskId, TaskPriority.valueOf(priority));
+
 	}
-	private static void updateTaskStatus(Scanner scan, TaskManagementSystem tms) {
-		try {
-			tms.getAllTasks();
-			System.out.println("Enter task id: ");
-			int taskId=scan.nextInt();
-			System.out.println("Enter task status: ");
-			System.out.println("ANALYSIS, DEVELOPMENT, TEST");
-			String status=scan.next();
-			tms.updateStatus(taskId, TaskStatus.valueOf(status));
-		} catch (Exception e) {
-			System.out.println("Yalnış giriş yaptınız. Çıkışa yönlendiriliyorsunuz.");
- 			System.out.println("---------------------------------------");
-		}
-		
+
+	private static void updateTaskStatus(Scanner scan, TaskManagementSystem tms) throws TaskException {
+		tms.getAllTasks();
+		System.out.println("Enter task id: ");
+		int taskId = scan.nextInt();
+		System.out.println("Enter task status: ");
+		System.out.println("ANALYSIS, DEVELOPMENT, TEST");
+		String status = scan.next();
+		tms.updateStatus(taskId, TaskStatus.valueOf(status));
 	}
-	private static void updateTaskUser(Scanner scan, TaskManagementSystem tms) {
-		try {
-			tms.getAllTasks();
-			System.out.println("Enter task id: ");
-			int taskId=scan.nextInt();
-			tms.getAllUsers();
-			System.out.println("Enter user id: ");
-			int userId = scan.nextInt();
-			tms.updateUser(taskId, userId);
-		} catch (Exception e) {
-			System.out.println("Yalnış giriş yaptınız. Çıkışa yönlendiriliyorsunuz.");
- 			System.out.println("---------------------------------------");
-		}
-		
+
+	private static void updateTaskUser(Scanner scan, TaskManagementSystem tms) throws TaskException {
+		tms.getAllTasks();
+		System.out.println("Enter task id: ");
+		int taskId = scan.nextInt();
+		tms.getAllUsers();
+		System.out.println("Enter user id: ");
+		int userId = scan.nextInt();
+		tms.updateUser(taskId, userId);
 	}
+
 	private static int getUserIntInput(Scanner scan) {
 		try {
 			return scan.nextInt();
@@ -142,20 +136,18 @@ public class ManagementSystemMain {
 			return -1;
 		}
 	}
-	
 
-
-	/*tms.createUser("Burcak", UserType.DEVELOPER);
-	
-	tms.createUser("Canan", UserType.ANALYST);
-	tms.createUser("Sabiha",UserType.ANALYST);
-	tms.createUser("Gokce", UserType.TESTER);
-	tms.createTask("PAYE", 1, TaskStatus.ANALYSIS, TaskPriority.HIGH );
-	tms.createTask("NETMERA",3, TaskStatus.DEVELOPMENT, TaskPriority.HIGH );
-	tms.createTask("ADJUST", 2, TaskStatus.TEST, TaskPriority.HIGH );
-	tms.createTask("BURGER KING", 5, TaskStatus.DEVELOPMENT, TaskPriority.HIGH );
-	tms.createTask("FINANCELL", 4, TaskStatus.TEST, TaskPriority.HIGH );
-	tms.updatePriority(4, TaskPriority.LOW);
-	tms.updateStatus(4, TaskStatus.TEST);
-	tms.updateUser(2, 2);*/
+	/*
+	 * tms.createUser("Burcak", UserType.DEVELOPER);
+	 * 
+	 * tms.createUser("Canan", UserType.ANALYST);
+	 * tms.createUser("Sabiha",UserType.ANALYST); tms.createUser("Gokce",
+	 * UserType.TESTER); tms.createTask("PAYE", 1, TaskStatus.ANALYSIS,
+	 * TaskPriority.HIGH ); tms.createTask("NETMERA",3, TaskStatus.DEVELOPMENT,
+	 * TaskPriority.HIGH ); tms.createTask("ADJUST", 2, TaskStatus.TEST,
+	 * TaskPriority.HIGH ); tms.createTask("BURGER KING", 5, TaskStatus.DEVELOPMENT,
+	 * TaskPriority.HIGH ); tms.createTask("FINANCELL", 4, TaskStatus.TEST,
+	 * TaskPriority.HIGH ); tms.updatePriority(4, TaskPriority.LOW);
+	 * tms.updateStatus(4, TaskStatus.TEST); tms.updateUser(2, 2);
+	 */
 }
--- Example/src/ManagementSystem/TaskManagementSystem.java
+++ Example/src/ManagementSystem/TaskManagementSystem.java
@@ -7,91 +7,64 @@ public class TaskManagementSystem {
 	private static TaskManagementSystem INSTANCE = new TaskManagementSystem();
 	private Map<Integer, User> users;
 	private Map<Integer, Task> tasks;
-	
+
 	public static TaskManagementSystem getIntsance() {
 		return INSTANCE;
 	}
-	
+
 	private TaskManagementSystem() {
 		users = new HashMap<>();
 		tasks = new HashMap<>();
 	}
-	
+
 	public void createUser(String name, UserType type) {
 		int id = users.size();
 		users.put(id, new User(id, name, type));
 		System.out.println(users.get(id));
 	}
-	
-	public void createTask(String name,int userId, TaskStatus status,TaskPriority priority ) {
-		if(users.isEmpty())
-		{
-			System.out.println("Önce kullanıcı oluşturunuz.");
-			System.out.println("---------------------------------------");
-			return;
-		}
-		else if(!users.containsKey(userId))
-		{
-			System.out.println("Böyle bir kullanıcı yok");
-			System.out.println("---------------------------------------");
-			return;
-		}
+
+	public void createTask(String name, int userId, TaskStatus status, TaskPriority priority) throws TaskException {
+		if (users.isEmpty()) throw new TaskException(TypeError.NO_USER);
+		else if (!users.containsKey(userId)) throw new TaskException(TypeError.NO_USER_WITH_ID);
 		int id = tasks.size();
 		tasks.put(id, new Task(id, name, userId, status, priority));
 		System.out.println(tasks.get(id));
 	}
-	public void getAllUsers() {
-		if (users.isEmpty()) {
-			System.out.println("Kullanıcı Yok");
-			System.out.println("---------------------------------------");
-			return;
-		}
 
-		for(Map.Entry<Integer, User> e:users.entrySet()) {
+	public void getAllUsers() throws TaskException {
+		if (users.isEmpty()) throw new TaskException(TypeError.NO_USER);
+
+		for (Map.Entry<Integer, User> e : users.entrySet()) {
 			System.out.println(e.getValue());
 		}
 	}
-	
-	public void getAllTasks() {
-		if (tasks.isEmpty()) {
-			System.out.println("Task Yok");
-			System.out.println("---------------------------------------");
-			return;
-		}
 
-		for(Map.Entry<Integer, Task> e:tasks.entrySet()) {
+	public void getAllTasks() throws TaskException {
+		if (tasks.isEmpty()) throw new TaskException(TypeError.NO_TASK);
+
+		for (Map.Entry<Integer, Task> e : tasks.entrySet()) {
 			System.out.println(e.getValue());
-		}	
+		}
 	}
-	
-	public void updateUser(int id, int userId) {
-		if (!tasks.containsKey(id)) {
-			System.out.println("Task yok");
-			return;
-		} 
-		
+
+	public void updateUser(int id, int userId) throws TaskException {
+		if (!tasks.containsKey(id)) throw new TaskException(TypeError.NO_TASK_WITH_ID);
+
 		Task task = tasks.get(id);
 		task.setUser(userId);
 		System.out.println(task);
 	}
-	
-	public void updateStatus(int id,TaskStatus status) {
-		if (!tasks.containsKey(id)) {
-			System.out.println("Task yok");
-			System.out.println("---------------------------------------");
-			return;
-		} 
-		
+
+	public void updateStatus(int id, TaskStatus status) throws TaskException {
+		if (!tasks.containsKey(id)) throw new TaskException(TypeError.NO_TASK_WITH_ID);
+
 		Task task = tasks.get(id);
 		task.setStatus(status);
 		System.out.println(task);
 	}
-	public void updatePriority(int id,TaskPriority priority) {
-		if (!tasks.containsKey(id)) {
-			System.out.println("Task yok");
-			System.out.println("---------------------------------------");
-			return;
-		} 
+
+	public void updatePriority(int id, TaskPriority priority) throws TaskException {
+		if (!tasks.containsKey(id)) throw new TaskException(TypeError.NO_TASK_WITH_ID);
 
 		Task task = tasks.get(id);
 		task.setPriority(priority);
@@ -101,6 +74,5 @@ public class TaskManagementSystem {
 	public User getUserbyId(int i) {
 		return users.get(i);
 	}
-
 
 }
